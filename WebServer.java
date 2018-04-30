b9fd843a886dc7d36d12ae460bbb98b4a1f2442c
Aaron <8881619+acai422@users.noreply.github.com>
2018-04-30 02:08:00 -0400

    Add files via upload

---
+++ WebServer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webserver;
+
+/**
+ *
+ * @author aaron
+ * @UGA ID: 810379273
+ */
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public final class WebServer {
+
+    public static void main(String[] args) throws Exception {
+        int port = 6789;
+        ServerSocket socket1 = new ServerSocket(port);
+        
+        while(true){
+            Socket socket2 = socket1.accept(); 
+            HttpRequest request = new HttpRequest(socket2);
+            Thread thread  = new Thread(request);            
+            thread.start();
+        }    
+    }
+}
+
